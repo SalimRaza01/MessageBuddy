42e8248bf43fd97677a9cbda54e8e832028d69f4
Salim Raza <Salim.creation2017@gmail.com>
2022-07-22 11:41:51 +0530

    addded final

---
+++ src/misc/firebase.js
@@ -0,0 +1,40 @@
+import { Notification as Toast } from 'rsuite';
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getDatabase } from 'firebase/database';
+import { getStorage } from 'firebase/storage';
+import { getMessaging, isSupported, onMessage } from 'firebase/messaging';
+import { getFunctions, connectFunctionsEmulator } from 'firebase/functions';
+import { isLocalhost } from './helpers';
+
+const config = {
+  apiKey: 'AIzaSyD9eJZcS-jZbhbSZt65OWzepOwX99jkpUM',
+  authDomain: 'message-buddy-e9efb.firebaseapp.com',
+  databaseURL: 'https://message-buddy-e9efb-default-rtdb.firebaseio.com',
+  projectId: 'message-buddy-e9efb',
+  storageBucket: 'message-buddy-e9efb.appspot.com',
+  messagingSenderId: '332050796865',
+  appId: '1:332050796865:web:279e3d6bc40f75b98991c0',
+};
+
+export const fcmVapidKey =
+  'BLs_I-HQyrAuUJJh8H3U0vtHGhVhXLMqoVoomeNL90GMKm0-o7sSoN9CJYRiBAVz-Yi7ZAni8mKateJfDwodTnw';
+
+const app = initializeApp(config);
+export const auth = getAuth(app);
+export const database = getDatabase(app);
+export const storage = getStorage(app);
+export const functions = getFunctions(app, 'europe-west3');
+
+export const messaging = isSupported() ? getMessaging(app) : null;
+
+if (messaging) {
+  onMessage(messaging, ({ notification }) => {
+    const { title, body } = notification;
+    Toast.info({ title, description: body, duration: 0 });
+  });
+}
+
+if (isLocalhost) {
+  connectFunctionsEmulator(functions, 'localhost', 5001);
+}
